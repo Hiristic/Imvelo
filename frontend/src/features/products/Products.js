eb80d5c3114b39fa604217935befd914a0e51eda
Armin Salcin <arminsalcin@Armins-MacBook-Pro.local>
2021-08-31 10:08:24 +0200

    started tree menu

--- frontend/src/features/products/Products.js
+++ frontend/src/features/products/Products.js
@@ -6,6 +6,7 @@ import { ReportsColumns } from "../../components/CustomTable/columnsConstants";
 import Pic1 from "../../assets/images/pictogram1.svg";
 import Pic2 from "../../assets/images/pictogram2.svg";
 import Pic3 from "../../assets/images/pictogram3.svg";
+import TreeMenu from "react-simple-tree-menu";
 
 const Container = styled.div`
   width: 100%;
@@ -36,6 +37,71 @@ const data = [
 
 const SubRow = () => <div>opened</div>;
 
+const treeData = [
+  {
+    key: "first-level-node-1",
+    label: "Node 1 at the first level",
+    nodes: [
+      {
+        key: "second-level-node-1",
+        label: "Node 1 at the second level",
+        nodes: [
+          {
+            key: "third-level-node-1",
+            label: "Last node of the branch",
+            nodes: [], // you can remove the nodes property or leave it as an empty array
+          },
+        ],
+      },
+    ],
+  },
+  {
+    key: "first-level-node-2",
+    label: "Node 2 at the first level",
+  },
+];
+
+const DEFAULT_PADDING = 16;
+const ICON_SIZE = 8;
+const LEVEL_SPACE = 16;
+
+const ItemComponent = ({
+  level = 0,
+  hasNodes,
+  isOpen,
+  label,
+  searchTerm,
+  openNodes,
+  toggleNode,
+  matchSearch,
+  focused,
+  ...props
+}) => (
+  <div
+    {...props}
+    style={{
+      paddingLeft: DEFAULT_PADDING + ICON_SIZE + level * LEVEL_SPACE,
+      cursor: "pointer",
+      boxShadow: focused ? "0px 0px 5px 0px #222" : "none",
+      zIndex: focused ? 999 : "unset",
+      position: "relative",
+    }}
+  >
+    {hasNodes && (
+      <div
+        style={{ display: "inline-block" }}
+        onClick={(e) => {
+          hasNodes && toggleNode && toggleNode();
+          e.stopPropagation();
+        }}
+      >
+        {/*<ToggleIcon on={isOpen} />*/}
+      </div>
+    )}
+    {label}
+  </div>
+);
+
 const Products = () => {
   const handleChangeSelection = useCallback((count) => {
     console.log(count);
@@ -43,7 +109,13 @@ const Products = () => {
 
   return (
     <Container>
-      <div />
+      <TreeMenu data={treeData} hasSearch={false}>
+        {({ items }) => {
+          items.map(({ key, ...props }) => (
+            <ItemComponent key={key} {...props} />
+          ));
+        }}
+      </TreeMenu>
       <div>
         <DarkTableStyle>
           <Table
